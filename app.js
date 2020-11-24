7ae63630997dd84f59a873326e1f3d24230ea6c5
King1419 <abdullah.albagshi@gmail.com>
2020-11-24 20:02:50 +0300

    Complete V1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash')
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,22 +13,59 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const posts = []
+
+
+app.get('/', (req, res) => {
+    res.render('home', { text: homeStartingContent, posts: posts })
+
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('about', { text: aboutContent })
+})
+
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { text: contactContent })
+})
+
+app.get('/compose', (req, res) => {
+    res.render('compose')
+})
+
+app.post('/compose', (req, res) => {
+
+    posts.push({
+        title: req.body.postTitle,
+        body: req.body.postBody
+    })
+    res.redirect('/')
+})
+
+app.get('/post/:postName', (req, res) => {
+    let requestedPost = _.lowerCase(req.params.postName);
+
+
+    posts.forEach(post => {
+        let postTitle = _.lowerCase(post.title)
+        if (postTitle === requestedPost) {
+            res.render('post', { title: post.title, body: post.body })
+        } else {
+            console.log('not Found');
+        }
+    })
+})
 
 
 
 
+const PORT = process.env.PORT || 3000
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+    console.log(`Lisetining to http://localhost:${PORT}`);
 });
